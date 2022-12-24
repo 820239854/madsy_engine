eabefe37ea14274a7fe392dbab901ba98afd3c58
Archer <820239854@qq.com>
2022-12-24 12:03:05 +0800

    初始化窗口绘制

--- src/Core/Engine.h
+++ src/Core/Engine.h
@@ -2,29 +2,36 @@
 #define ENGINE_H
 
 #include "SDL.h"
+#include "SDL_image.h"
 
-class Engine
-{
-public:
-    static Engine *GetInstance()
-    {
-        return s_Instance = (s_Instance != nullptr) ? s_Instance : new Engine();
-    }
+#define SCREEN_WIDTH 960
+#define SCREN_HEIGHT 640
 
-    bool Init();
-    bool Clean();
-    void Quit();
+class Engine {
 
-    void Update();
-    void Render();
-    void Events();
+    public:
+        static Engine* GetInstance(){
+            return s_Instance = (s_Instance != nullptr)? s_Instance : new Engine();
+        }
 
-    inline bool IsRunning() { return m_IsRunning; }
+        bool Init();
+        bool Clean();
+        void Quit();
 
-private:
-    Engine() {}
-    bool m_IsRunning;
-    static Engine *s_Instance;
+        void Update();
+        void Render();
+        void Events();
+
+        inline bool IsRunning(){return m_IsRunning;}
+        inline SDL_Renderer* GetRenderer(){return m_Renderer;}
+
+    private:
+        Engine(){}
+        bool m_IsRunning;
+
+        SDL_Window* m_Window;
+        SDL_Renderer* m_Renderer;
+        static Engine* s_Instance;
 };
 
 #endif // ENGINE_H
