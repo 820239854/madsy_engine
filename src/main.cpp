8bdba827b3dcaf4f2eed753f70b552aedba6e9d1
Archer <820239854@qq.com>
2022-12-24 11:44:22 +0800

    初始化引擎

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "Core/Engine.h"
+
+int main(int argc, char* args[])
+{
+	Engine::GetInstance()->Init();
+	while(Engine::GetInstance()->IsRunning()){
+		Engine::GetInstance()->Events();
+		Engine::GetInstance()->Update();
+		Engine::GetInstance()->Render();
+	}
+	Engine::GetInstance()->Clean();
+	return 0;
+}
